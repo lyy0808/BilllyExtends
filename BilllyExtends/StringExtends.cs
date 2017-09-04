b055249040d183284a423d69b7f843deccd934fa
Billly <lyy0808@yeah.net>
2017-09-04 08:38:07 +0800

    添加String的扩展

---
+++ BilllyExtends/StringExtends.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BilllyExtends
+{
+   ///=================================================================================================
+   /// <summary>    A string extends. </summary>
+   ///
+   /// <remarks>    lyy, 2017:9:4_8:37:41. </remarks>
+   ///-------------------------------------------------------------------------------------------------
+
+   public static class StringExtends
+    {
+
+    }
+}
